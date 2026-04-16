59b127796614820d9c9d68910bb7f183a61210d5
Dedes <federico.lepore73@gmail.com>
2026-04-16 22:37:39 +0200

    new repo

---
+++ src/main/java/federicolepore/u5w19d5/U5w19d5Application.java
@@ -0,0 +1,13 @@
+package federicolepore.u5w19d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w19d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w19d5Application.class, args);
+	}
+
+}
