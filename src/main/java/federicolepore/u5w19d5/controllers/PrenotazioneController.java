ca12b307a1f7c59003859de5a601d75b5b1cf8da
Dedes <federico.lepore73@gmail.com>
2026-04-17 15:47:02 +0200

    Prenotazione save

---
+++ src/main/java/federicolepore/u5w19d5/controllers/PrenotazioneController.java
@@ -0,0 +1,41 @@
+package federicolepore.u5w19d5.controllers;
+
+import federicolepore.u5w19d5.entities.Prenotazione;
+import federicolepore.u5w19d5.exceptions.ValidationException;
+import federicolepore.u5w19d5.payloads.NewPrenotazioneDTO;
+import federicolepore.u5w19d5.payloads.PrenotazioneDTO;
+import federicolepore.u5w19d5.services.PrenotazioneService;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/prenotazioni")
+public class PrenotazioneController {
+
+    private final PrenotazioneService prenotazioneService;
+
+
+    public PrenotazioneController(PrenotazioneService prenotazioneService) {
+        this.prenotazioneService = prenotazioneService;
+    }
+
+
+    // 1 - POST http://localhost:3002/prenotazioni + @RequestBody
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) //quindi status 201
+    public NewPrenotazioneDTO newPrenotazione(@RequestBody @Validated PrenotazioneDTO body, BindingResult validationResult) {
+
+        if (validationResult.hasErrors()) {
+            List<String> errors = validationResult.getFieldErrors().stream().map(e -> e.getDefaultMessage()).toList();
+            throw new ValidationException(errors);
+        }
+        Prenotazione newP = this.prenotazioneService.save(body);
+        return new NewPrenotazioneDTO(newP.getPrenotazioneID());
+    }
+
+
+}
--- src/main/java/federicolepore/u5w19d5/services/PrenotazioneService.java
+++ src/main/java/federicolepore/u5w19d5/services/PrenotazioneService.java
@@ -1,8 +1,16 @@
 package federicolepore.u5w19d5.services;
 
+import federicolepore.u5w19d5.entities.Dipendente;
+import federicolepore.u5w19d5.entities.Prenotazione;
+import federicolepore.u5w19d5.exceptions.BadRequestException;
+import federicolepore.u5w19d5.payloads.PrenotazioneDTO;
 import federicolepore.u5w19d5.repositories.PrenotazioneRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Service
 public class PrenotazioneService {
 
@@ -18,8 +26,22 @@ public class PrenotazioneService {
         this.viaggioService = viaggioService;
     }
 
-//    public Prenotazione save(PrenotazioneDTO body) {
-//        // richiamo il dipendente e il viaggio poer i controlli
-//        Dipendente d = dipendenteService.findByID(body.dipendenteID());
-//    }
+
+    //1
+    public Prenotazione save(PrenotazioneDTO body) {
+        // richiamo il dipendente e il viaggio poer i controlli
+        Dipendente d = body.dipendente();
+        LocalDate dataV = body.viaggio().getData();
+
+        if (this.prenotazioneRepository.existsByDipendenteAndViaggio_Data(d, dataV)) {
+            throw new BadRequestException("Esiste già una prenotazione per il dipendente " + body.dipendente().getDipendenteID() + " per la data " + dataV);
+        }
+
+        Prenotazione p = new Prenotazione(body.dipendente(), body.preferenze(), body.viaggio());
+        Prenotazione savedP = this.prenotazioneRepository.save(p);
+        log.info("La prenotazione " + savedP.getPrenotazioneID() + " è avvenuta con successo");
+        return savedP;
+    }
+
+
 }
